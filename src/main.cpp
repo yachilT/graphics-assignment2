b138b55f3421145086c546a2dd6ee262f3369d69
yachilT <128305238+yachilT@users.noreply.github.com>
2024-12-08 12:11:21 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//if you wish to run with makefile extantion, try removinf the 2 defines below.
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+#include <cmath>
+#include <iostream>
+
+
+
+
+int main(void)
+{
+
+    return 0;
+}
