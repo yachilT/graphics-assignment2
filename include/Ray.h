f08977c6c57953d11a94c6a46bafef2567373d6b
yachilT <128305238+yachilT@users.noreply.github.com>
2024-12-09 19:03:39 +0200

    finished lights

--- include/Ray.h
+++ include/Ray.h
@@ -3,17 +3,17 @@
 
 class Ray {
    public:
-      glm::vec3* pos;
-      glm::vec3* dir;
+      glm::vec3 pos;
+      glm::vec3 dir;
 
       Ray(float x1, float y1, float z1, float x2, float y2, float z2){
-         this->pos = new glm::vec3(x1, y1, z1);
-         this->dir = new glm::vec3(x2, y2, z2);
+         this->pos = glm::vec3(x1, y1, z1);
+         this->dir = glm::vec3(x2, y2, z2);
       }
 
       Ray(glm::vec3& pos, glm::vec3& dir){
-         this->pos = new glm::vec3(pos.x, pos.y, pos.z);
-         this->dir = new glm::vec3(dir.x, dir.y, dir.z);
+         this->pos = glm::vec3(pos.x, pos.y, pos.z);
+         this->dir = glm::vec3(dir.x, dir.y, dir.z);
       }
       
 };
--- include/lights.h
+++ include/lights.h
@@ -2,37 +2,46 @@
 #include <glm/glm.hpp>
 #include "Ray.h"
 
+//TODO: consider specular and diffuse according to formula.
 class Light {
    public:
-      Light(glm::vec3 intensity);
-      virtual glm::vec3 getInensity(Ray r) = 0;
+      Light(glm::vec3 intensity, float specularExp);
+      virtual glm::vec3 diffuse(const Ray &normal) = 0;
+      virtual glm::vec3 specular(const Ray &normal, const glm::vec3 &viewDir) = 0;
       
    protected:
       glm::vec3 intensity;
+      float specularExp;
       
 };
 
-class Ambient: public Light {
+class Ambient {
    public:
-      Ambient(glm::vec3 intensity);
-      glm::vec3 getInensity(Ray r) override;
+      Ambient(glm::vec3 intensity, glm::vec3 k_ambient);
+      glm::vec3 getIntensity(const Ray &r);
+   private:
+      glm::vec3 intensity;
+      glm::vec3 k_ambient;
 };
 
 class Directional: public Light {
    public:
-      Directional(glm::vec3 intensity, glm::vec3 dir);
-      glm::vec3 getInensity(Ray r) override;
-   private:
+      Directional(glm::vec3 intensity, float specular_exp, glm::vec3 dir);
+      glm::vec3 diffuse(const Ray &normal) override;
+      glm::vec3 specular(const Ray &normal, const glm::vec3 &viewDir) override;
+   protected:
       glm::vec3 dir;
 };
 
-class Spotlight: public Light {
+class Spotlight: public Directional {
    public:
-      Spotlight(glm::vec3 intensity, glm::vec3 dir, glm::vec3 pos);
-      glm::vec3 getInensity(Ray r) override;
+      Spotlight(glm::vec3 intensity, float specular_exp, glm::vec3 dir, glm::vec3 pos, float cutoffCos);
+      glm::vec3 diffuse(const Ray &normal) override;
+      glm::vec3 specular(const Ray &normal, const glm::vec3 &viewDir) override;
    private:
-      glm::vec3 dir;
       glm::vec3 pos;
       float cutoffCos;
+
+      bool insideBeam(glm::vec3 p);
 };
 
