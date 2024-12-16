4acc6a7f2a9a62ce8f0fbe48828de04f41f06de5
FoyerD <amitfoye@post.bgu.ac.il>
2024-12-16 12:24:13 +0200

    made reader simpler

--- src/reader.cpp
+++ src/reader.cpp
@@ -16,35 +16,8 @@ Reader::Reader(const string &path) : lines()
 //TODO!: USE SSCANF
 ReaderLine Reader::proccesLine(const string &l)
 {
-    vector<string> tokens;
-    istringstream stream(l);
-    string tok;
-    while (stream >> tok) {
-        tokens.push_back(tok);
-    }
-
-    if (tokens.size() != 4) {
-        cout << l + " has more or less than 4 values" << endl;
-        throw exception();
-    }
     ReaderLine r;
-    
-    if (tokens[0].size() != 1) {
-        cout << tokens[0] + " isn't a single char";
-        throw exception();
-    }
-
-    r.c = tokens[0][0];
-
-    for (int i = 1; i < 4; i++) {
-        try {
-            r.fs[i] = stof(tokens[i]);
-        }  catch (const std::invalid_argument& e) {
-            cerr << "Error: Unable to convert string to float. Details: " << e.what() << endl;
-            throw exception();
-        }
-    }
-
+    sscanf(l.c_str(), "%c %f %f %f %f", &r.c, r.fs, r.fs + 1, r.fs + 2, r.fs + 3, r.fs + 4);
     return r;
 }
 
